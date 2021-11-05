2c3229cafbd36745e4030af8ddb81d754688105c
jbuget <jbuget@gmail.com>
2021-11-05 19:05:06 +0100

    Declare Environment config and use it in app components

--- src/environment.ts
+++ src/environment.ts
@@ -1,7 +1,36 @@
-const environment = {
+type Environment = {
   server: {
-    port: process.env.PORT || 3000,
+    port: number;
+    host: string;
+  },
+  logger: {
+    enabled: boolean;
+    level: string;
+  }
+}
+
+function getNumber(value: string | undefined, defaultValue: number): number {
+  if (value) {
+    return parseInt(value);
+  }
+  return defaultValue;
+}
+
+function getBoolean(value: string | undefined, defaultValue: boolean): boolean {
+  if (value) {
+    return value === 'true';
+  }
+  return defaultValue;
+}
+
+const environment: Environment = {
+  server: {
+    port: getNumber(process.env.PORT, 3000),
     host: process.env.HOST || 'localhost'
+  },
+  logger: {
+    enabled: getBoolean(process.env.LOGGER_ENABLED, true),
+    level: process.env.LOGGER_LEVEL || 'info'
   }
 };
 
--- src/logger.ts
+++ src/logger.ts
@@ -1,7 +1,10 @@
 import pino, { P } from 'pino';
+import { environment } from './environment';
 
 function build(): P.Logger {
   return pino({
+    enabled: environment.logger.enabled,
+    level: environment.logger.level,
     transport: {
       target: 'pino-pretty'
     }
