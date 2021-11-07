7da353e605cfe21d91b0cdfbbcc744150ac1483e
jbuget <jbuget@gmail.com>
2021-11-08 00:12:12 +0100

    Add Prisma (with prisma-client-js)

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,8 @@
 import fastify, { FastifyInstance } from 'fastify';
 import { P } from 'pino';
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
 
 function build(logger?: P.Logger): FastifyInstance {
   const server = fastify({
@@ -8,6 +11,14 @@ function build(logger?: P.Logger): FastifyInstance {
 
   server.get('/ping', async () => {
     return 'pong\n';
+  });
+
+  server.get('/todos', async () => {
+    try {
+      return await prisma.todo.findMany();
+    } finally {
+      await prisma.$disconnect();
+    }
   });
 
   return server;
