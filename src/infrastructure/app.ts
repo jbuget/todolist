37fda11c992cbfaca7318254791afb2335222461
jbuget <jbuget@gmail.com>
2021-11-16 01:39:21 +0100

    Design /tasks endpoints

--- src/infrastructure/app.ts
+++ src/infrastructure/app.ts
@@ -13,12 +13,43 @@ function build(logger?: P.Logger): FastifyInstance {
     return 'pong\n';
   });
 
+  // Get all tasks
   server.get('/tasks', async () => {
     try {
       return await prisma.task.findMany();
     } finally {
       await prisma.$disconnect();
     }
+  });
+
+  // Crate a new task
+  server.post('/tasks', async (request, reply) => {
+    reply.code(501).send();
+  });
+
+  // Get a task
+  server.get('/tasks/:id', async (request, reply) => {
+    reply.code(501).send();
+  });
+
+  // Update a task
+  server.post('/tasks/:id', async (request, reply) => {
+    reply.code(501).send();
+  });
+
+  // Close a task
+  server.post('/tasks/:id/close', async (request, reply) => {
+    reply.code(501).send();
+  });
+
+  // Reopen a task
+  server.post('/tasks/:id/reopen', async (request, reply) => {
+    reply.code(501).send();
+  });
+
+  // Delete a task
+  server.delete('/tasks/:id', async (request, reply) => {
+    reply.code(501).send();
   });
 
   return server;
