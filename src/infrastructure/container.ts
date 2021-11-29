aa35c5bef88677ec3854f918e059256451000e52
jbuget <jbuget@gmail.com>
2021-11-29 19:26:05 +0100

    Change injection mode: use CLASSIC instead of PROXY (default) because we are in a Node app

--- src/infrastructure/container.ts
+++ src/infrastructure/container.ts
@@ -1,9 +1,13 @@
-import { createContainer, asClass } from 'awilix';
+import { asClass, createContainer } from 'awilix';
 import { TaskRepositorySql } from './repositories/TaskRepositorySql';
+import { PrismaClient } from '@prisma/client';
 
-const container = createContainer();
+const container = createContainer({
+  injectionMode: 'CLASSIC'
+});
 
 container.register({
+  prisma: asClass(PrismaClient),
   taskRepository: asClass(TaskRepositorySql)
 });
 
